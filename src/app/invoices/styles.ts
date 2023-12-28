684f9809d5aec48bc0c3660981c2eb859261e258
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-12-28 17:14:02 -0300

    feat: added new page for invoices

--- src/app/invoices/styles.ts
+++ src/app/invoices/styles.ts
@@ -3,5 +3,49 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  color: var(--white-200);
+  width: 100%;
+  padding: 8rem 2rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const StyledDiv = styled.div`
+  width: 18.75rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+`;
+
+export const TableContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  svg {
+    width: 1rem;
+    height: 1rem;
+    font-size: 2.5rem;
+    color: var(--green-dark);
+  }
+`;
+
+export const StyledTitle = styled.h1`
+  font-size: 1.375rem;
+  text-align: center;
+  color: var(--green-dark);
+  font-weight: 600;
+`;
+
+export const ToggleSection = styled.section`
+  width: 100%;
+  display: flex;
+  align-items: baseline;
+  justify-content: flex-end;
+`;
+
+export const StyledH1 = styled.h1`
+  text-align: center;
+  padding-bottom: 3rem;
 `;
---
+++ src/components/card/index.tsx
@@ -0,0 +1,101 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { MdPageview } from "react-icons/md";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { ApiResponse } from "@/interfaces/api-response.interface";
+
+interface DataGridProps {
+  invoices: ApiResponse;
+}
+
+export default function InvoicesTable({ invoices }: DataGridProps) {
+  const columns: GridColDef[] = [
+    {
+      field: "clientNumber",
+      headerName: "n. do cliente",
+      width: 190,
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "year",
+      headerName: "mês/ano",
+      editable: true,
+      width: 190,
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "contribution",
+      headerName: "contribuição(kWh)",
+      editable: true,
+      width: 190,
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "electricity",
+      headerName: "energia-elétrica(kWh)",
+      type: "number",
+      editable: true,
+      width: 190,
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "total",
+      headerName: "total(R$)",
+      width: 190,
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "billUrl",
+      headerName: "invoice",
+      width: 190,
+      editable: false,
+      renderCell: (params) => (
+        <a href={params.value} target="_blank" rel="noopener noreferrer">
+          <MdPageview />
+        </a>
+      ),
+    },
+  ];
+
+  const rows = invoices.data.map((invoice) => {
+    return {
+      id: invoice.id,
+      clientNumber: invoice.clientNumber,
+      year: `${invoice.month}/${invoice.year}`,
+      contribution: invoice.contribution,
+      electricity: invoice.electricity,
+      total: invoice.total,
+      billUrl: invoice.billUrl,
+    };
+  });
+
+  return (
+    <Box
+      sx={{
+        width: "80%",
+        display: "flex",
+        justifyContent: "space-around",
+      }}
+    >
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: invoices.pageSize,
+            },
+          },
+        }}
+        pageSizeOptions={[invoices.pageSize]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
+  );
+}
